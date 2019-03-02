06e71be6b9704f50ba15e05e707f597f10282da8
AKASH JAIN <akash19jain@gmail.com>
2019-03-02 10:16:45 +0530

    Create CHFIDEAL.C

---
+++ CODECHEF/DEC18A/CHFIDEAL.C
@@ -0,0 +1,19 @@
+https://www.codechef.com/DEC18A/problems/CHFIDEAL/
+
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("1\n");
+    fflush(stdout);
+    long long x;
+    scanf("%lld",&x);
+    if(x==2)
+     printf("3\n");   
+        else
+      printf("2\n");  
+    fflush(stdout);
+}
