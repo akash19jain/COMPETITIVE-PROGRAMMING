cf4a5754996947fb460516e82b873e625906b19f
AKASH JAIN <akash19jain@gmail.com>
2019-03-02 12:31:56 +0530

    Create CHFTIRED.C

---
+++ CODECHEF/NOV18B/CHFTIRED.C
@@ -0,0 +1,15 @@
+https://www.codechef.com/NOV18B/problems/CHFTIRED/
+
+
+#include<stdio.h>
+int main()
+{
+long long t;
+scanf("%lld",&t);
+while(t--)
+{
+long long a,b;
+scanf("%lld%lld",&a,&b);
+printf("YES\n");
+}
+}
