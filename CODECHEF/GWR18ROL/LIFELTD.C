81ce2a05caaf5d4608cd6ba0a6f47397d016578f
AKASH JAIN <akash19jain@gmail.com>
2019-03-02 10:13:11 +0530

    Create LIFELTD.C

---
+++ CODECHEF/GWR18ROL/LIFELTD.C
@@ -0,0 +1,42 @@
+https://www.codechef.com/GWR18ROL/problems/LIFELTD/
+
+/*  AUTHOR:AKASH JAIN
+*   USERNAME:akash19jain    
+*   DATE;18/12/2018    
+*/
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+    long long t;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        char arr[4][4];
+        for(int i=0;i<3;i++)
+        {
+            scanf("%s", arr[i]);
+        }
+        int flag=0;
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                if(arr[i][j]=='l' && arr[i+1][j]=='l' && arr[i+1][j+1]=='l')
+                {
+                    flag=1;
+                    break;
+                }
+            }
+        }
+        if(flag==1)
+        printf("yes\n");
+        else
+        printf("no\n");
+    }
+
+    return 0;
+}
