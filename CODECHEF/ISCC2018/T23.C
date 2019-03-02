2d228b6719229ca9426c2e8b688b959a83a57d89
AKASH JAIN <akash19jain@gmail.com>
2019-03-02 22:42:04 +0530

    Create T23.C

---
+++ CODECHEF/ISCC2018/T23.C
@@ -0,0 +1,38 @@
+https://www.codechef.com/ISCC2018/problems/T23/
+
+#include<stdio.h>
+
+int gcd(int a,int b)
+{
+    if(b==0)
+    return a;
+        
+    return gcd(b,a%b);
+        
+}
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,i,j,gc=0;
+        scanf("%d",&n);
+        
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&j);
+            gc=gcd(gc,j);
+        }
+        
+        if(gc==1)
+        printf("YES\n");
+        else
+        printf("NO\n");
+    
+    }
+    
+   
+    return 0;
+}
