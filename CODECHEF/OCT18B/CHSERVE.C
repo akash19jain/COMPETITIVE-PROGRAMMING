7c9524a9ed2d2c1252ade119eb108813667adb30
AKASH JAIN <akash19jain@gmail.com>
2019-03-02 12:36:50 +0530

    Create CHSERVE.C

---
+++ CODECHEF/OCT18B/CHSERVE.C
@@ -0,0 +1,23 @@
+https://www.codechef.com/OCT18B/problems/CHSERVE/
+
+#include<stdio.h>
+int main()
+{
+    long long t;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        long long p1,p2,k;
+        scanf("%lld%lld%lld",&p1,&p2,&k);
+        
+        long long sum=(p1+p2)/k;
+        int sum1=sum%2;
+        
+        if(sum1==1)
+        printf("COOK\n");
+        else
+        printf("CHEF\n");
+        
+    }
+    return 0;
+}
