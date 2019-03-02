b6a5c89c3d4488025000a380738cb3a9b34abec5
AKASH JAIN <akash19jain@gmail.com>
2019-03-02 22:45:06 +0530

    Create MGCSET.C

---
+++ CODECHEF/JULY18B/MGCSET.C
@@ -0,0 +1,31 @@
+https://www.codechef.com/JULY18B/problems/MGCSET/
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,m;
+        scanf("%d%d",&n,&m);
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+         
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]%m==0)
+            count++;
+            
+        }
+        long int ans=pow(2,count)-1;
+        
+        printf("%ld\n",ans);
+    }
+    return 0;
+}
