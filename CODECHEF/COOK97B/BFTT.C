7f846a08646ccddc04f77052d8ad3513238ddeb1
AKASH JAIN <akash19jain@gmail.com>
2019-03-02 17:43:59 +0530

    Create BFTT.C

---
+++ CODECHEF/COOK97B/BFTT.C
@@ -0,0 +1,32 @@
+https://www.codechef.com/COOK97B/problems/BFTT/
+
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        long long n;
+        scanf("%lld",&n);
+        for(long long i=n+1;;i++)
+        {
+            int count=0;
+            long long x=i;
+            while(x>0)
+            {
+                int r=x%10;
+                if(r==3)
+                count++;
+                x=x/10;
+                if(count==3)
+                break;
+            }
+            if(count==3)
+            {
+                printf("%lld\n",i);
+                break;
+            }
+        }
+    }
+}
