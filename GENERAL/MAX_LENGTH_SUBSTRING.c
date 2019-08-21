db77252ada24a16e779885e005feb9271cfef35a
AKASH JAIN <akash19jain@gmail.com>
2019-08-21 16:52:17 +0530

    Maximum length substring having all same characters after k changes

---
+++ GENERAL/MAX_LENGTH_SUBSTRING.c
@@ -0,0 +1,108 @@
+//Maximum length substring having all same characters after k changes
+//We have a string of length n, which consist only UPPER and LOWER CASE characters and we have a number k (always less than n and greater than 0). We can make at most k changes in our string such that we can get a sub-string of maximum length which have all same characters.
+/*  AUTHOR:AKASH JAIN
+*   USERNAME:akash19jain    
+*   DATE:21/08/2019 
+*/
+//#include<algorithm>
+//using namespace std;
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define SC1(x)          scanf("%lld",&x)
+#define SC2(x,y)        scanf("%lld%lld",&x,&y)
+#define SC3(x,y,z)      scanf("%lld%lld%lld",&x,&y,&z)
+#define SCS(x)          scanf("\n%s", x)
+#define PF1(x)          printf("%lld\n",x)
+#define PF2(x,y)        printf("%lld %lld\n",x,y)
+#define PF3(x,y,z)      printf("%lld %lld %lld\n",x,y,z)
+#define REP(i,n)        for(long long i=0;i<(n);i++)
+#define FOR(i,a,b)      for(long long i=(a);i<=(b);i++)
+#define FORD(i,a,b)     for(long long i=(a);i>=(b);i--)
+#define WHILE(n)        while(n--)
+#define MEM(a, b)       memset(a, (b), sizeof(a))
+#define ITOC(c)         ((char)(((int)'0')+c))
+#define MID(s,e)        (s+(e-s)/2)
+#define SZ(a)            strlen(a)
+#define MOD             1000000007
+#define MAX             10000000005
+#define MIN             -10000000005
+#define PI              3.1415926535897932384626433832795
+#define DEB(x)          printf("The value of \"%s\" is: %d\n",#x,x)
+#define CASES           ll t;SC1(t);while(t--)
+#define ABS(a)          ((a>0)?a:-(a))
+
+const int INF = 1<<29;
+typedef long long ll;
+typedef unsigned long long ull;
+#define FILEIO(name) \  freopen(name".in", "r", stdin); \   freopen(name".out", "w", stdout);
+int cmp(const void * a,const void * b);
+long long maxv(long long a,long long b);
+long long minv(long long a,long long b);
+long long gcd(long long u,long long v);
+
+ll solve(char str[],ll n,ll k,char ch)
+{
+	ll max=1,count=0,l=0,r=0;
+
+	while(r<n)
+	{
+		if(str[r]!=ch)
+			count++;
+
+		while(count>k)
+		{
+			if(str[l]!=ch)
+				count--;
+			l++;
+		}
+		max=maxv(max,r-l+1);
+		r++;
+	}
+	return max;
+}
+char str[100005];
+int main()
+{
+	CASES
+	{
+		ll n,k;
+		SC2(n,k);
+		//char str[n+5];
+		SCS(str);
+		ll ans=1;
+		REP(i,26)
+		{
+			ans=maxv(ans,solve(str,n,k,i+'A'));
+			ans=maxv(ans,solve(str,n,k,i+'a'));
+		}
+		PF1(ans);
+	}
+    return 0;
+}
+ 
+ 
+//qsort(arr,n,sizeof(arr[0]),cmp);
+int cmp (const void * a, const void * b)
+{
+    if( *(ll*)a - *(ll*)b < 0 ) return -1;
+    if( *(ll*)a - *(ll*)b > 0 ) return 1;
+        return 0;
+}
+long long maxv(long long a,long long b)
+{
+    if(a>b) return a;
+        return b;
+}
+long long minv(long long a,long long b)
+{
+    if(a<b) return a;
+        return b;
+}
+long long gcd(long long u,long long v)
+{
+    if (v == 0) return u; 
+       return gcd(v, u%v);
+}
