6f35274ad919ef61a990858afdd20929b0eabb6b
AKASH JAIN <akash19jain@gmail.com>
2019-08-23 01:21:00 +0530

    HACKEREARTH SOLUTIONS GREEDY

---
+++ HACKEREARTH/MOTU_AND_PATLU.c
@@ -0,0 +1,122 @@
+/*  AUTHOR:AKASH JAIN
+*   USERNAME:akash19jain    
+*   DATE:22/08/2019 
+*/
+//#include<algorithm>
+//using namespace std;
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define SC1(x)          scanf("%lld",&x)
+#define SC2(x,y)        scanf("%lld%lld",&x,&y)
+#define SC3(x,y,z)      scanf("%lld%lld%lld",&x,&y,&z)
+#define SCS(x)          scanf("\n%s", x)
+#define PF1(x)          printf("%lld\n",x)
+#define PF2(x,y)        printf("%lld %lld\n",x,y)
+#define PF3(x,y,z)      printf("%lld %lld %lld\n",x,y,z)
+#define REP(i,n)        for(long long i=0;i<(n);i++)
+#define FOR(i,a,b)      for(long long i=(a);i<=(b);i++)
+#define FORD(i,a,b)     for(long long i=(a);i>=(b);i--)
+#define WHILE(n)        while(n--)
+#define MEM(a, b)       memset(a, (b), sizeof(a))
+#define ITOC(c)         ((char)(((int)'0')+c))
+#define MID(s,e)        (s+(e-s)/2)
+#define SZ(a)            strlen(a)
+#define MOD             1000000007
+#define MAX             10000000005
+#define MIN             -10000000005
+#define PI              3.1415926535897932384626433832795
+#define DEB(x)          printf("The value of \"%s\" is: %d\n",#x,x)
+#define CASES           ll t;SC1(t);while(t--)
+#define ABS(a)          ((a>0)?a:-(a))
+
+const int INF = 1<<29;
+typedef long long ll;
+typedef unsigned long long ull;
+#define FILEIO(name) \  freopen(name".in", "r", stdin); \   freopen(name".out", "w", stdout);
+int cmp(const void * a,const void * b);
+long long maxv(long long a,long long b);
+long long minv(long long a,long long b);
+long long gcd(long long u,long long v);
+
+int main()
+{
+	CASES
+	{
+		ll n;
+		SC1(n);
+		ll arr[n];
+		REP(i,n)
+		{
+			SC1(arr[i]);
+		}
+		if(n==1)
+		{
+			printf("1 0\nMotu\n");
+		}
+		else
+		{
+			ll i=0,j=n-1,sum1=0,sum2=0,flag=0;
+			while(i<=j)
+			{
+				if(sum1<=2*sum2)
+				{
+					while((sum1<=2*sum2) && i<=j)
+					{
+						sum1+=arr[i];
+						i++;
+					}
+				}
+				else
+				{
+					sum2+=arr[j];
+					j--;
+				}
+				
+			}
+			if(i>n-j-1)
+			{
+				printf("%lld %lld\n",i,n-j-1);
+				printf("Motu\n");
+			}
+			else if(i<n-j-1)
+			{
+				printf("%lld %lld\n",i,n-j-1);
+				printf("Patlu\n");
+			}
+			else
+			{
+				printf("%lld %lld\n",i,n-j-1);
+				printf("Tie\n");
+			}
+		}
+		
+	}
+    return 0;
+}
+ 
+ 
+//qsort(arr,n,sizeof(arr[0]),cmp);
+int cmp (const void * a, const void * b)
+{
+    if( *(ll*)a - *(ll*)b < 0 ) return -1;
+    if( *(ll*)a - *(ll*)b > 0 ) return 1;
+        return 0;
+}
+long long maxv(long long a,long long b)
+{
+    if(a>b) return a;
+        return b;
+}
+long long minv(long long a,long long b)
+{
+    if(a<b) return a;
+        return b;
+}
+long long gcd(long long u,long long v)
+{
+    if (v == 0) return u; 
+       return gcd(v, u%v);
+}
